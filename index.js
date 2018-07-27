37f51cd8c730243d5261d0fdd308d62c15a8fc77
hanyi <hanyitim@yeah.net>
2018-07-27 20:18:46 +0800

    添加mocha

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import browser from './lib/browser/index.js';
 import util from './lib/util/index.js';
 import debug from './lib/debug/index.js';
+import timer from './lib/timer/index.js';
 
 module.exports = {
     browser,
     ...debug,
-    ...util
+    ...util,
+    ...timer
 }
